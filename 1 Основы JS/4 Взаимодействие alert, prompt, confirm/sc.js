1ba07b84e9ed2fc9d43ae80316b946311e92225c
Al <aliastr.respiro@gmail.com>
2023-02-28 12:21:41 +0600

    Initial commit

---
+++ 1 Основы JS/4 Взаимодействие alert, prompt, confirm/sc.js
@@ -0,0 +1,11 @@
+//Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+
+<!DOCTYPE html>
+<html>
+<body>
+    <script>
+        let name = prompt("Ваше имя?", "");
+        alert(name);
+    </script>
+</body>
+</html>
